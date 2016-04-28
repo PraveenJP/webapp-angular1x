2cda691bf4c97785066a013c2e97b7cc5423e97b
praveenjp <roadizpraveen@gmail.com>
2016-04-28 23:34:55 +0530

    layout design and router setup

--- app/js/app.js
+++ app/js/app.js
@@ -4,9 +4,29 @@ var app = angular.module('app',['ui.router']);
 app.config(function($stateProvider,$urlRouterProvider){
     $stateProvider
     .state('home',{
-        url:'/home',
+        url:'/',
         templateUrl:'templates/home.html'
     })
+    .state('md6',{
+        url:'/md6',
+        templateUrl:'templates/md6.html'
+    })
+    .state('about',{
+        url:'/about',
+        templateUrl:'templates/about.html'
+    })
+    .state('shop',{
+        url:'/shop',
+        templateUrl:'templates/shop.html'
+    })
+    .state('contact',{
+        url:'/contact',
+        templateUrl:'templates/contact.html'
+    })
+    .state('volounteer',{
+        url:'/volounteer',
+        templateUrl:'templates/volounteer.html'
+    });
     
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/');
 });
