b11e3ff281de7de458d7f9bc87424e4da5ac07a2
praveenjp <roadizpraveen@gmail.com>
2016-05-01 11:10:20 +0530

    about page updated

--- app/js/controllers/about.controller.js
+++ app/js/controllers/about.controller.js
@@ -1,3 +1,9 @@
 app.controller('about',function($scope,webservice){
     
+    $scope.getAboutContent = function(){
+        webservice.getContent().then(function(response){
+           $scope.aboutContent = response.data[1].about;          
+        });
+    }
+    
 })
